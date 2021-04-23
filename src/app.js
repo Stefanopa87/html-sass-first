0e046ea75043635f7ee5d6ae4786ba58ac2aaf29
Stefano Patti <mastepa@hotmail.com>
2021-04-23 17:50:57 +0200

    es done

---
+++ src/app.js
@@ -0,0 +1,21 @@
+// src/app.js
+function init() {
+
+    console.log('src/app.js AGGANCIATO');
+
+
+    new Vue({
+        el: "#app"
+
+    //     data: {
+    //     },
+
+    //     methods: {            
+    //     },
+
+    //     mounted() {            
+    //     },
+    });
+}
+
+document.addEventListener('DOMContentLoaded', init);
